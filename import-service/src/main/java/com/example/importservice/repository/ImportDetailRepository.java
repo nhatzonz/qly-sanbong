6db26df6e49036ff6446c63f87c8fc771ded2a05
nhatzonz <156907693+nhatzonz@users.noreply.github.com>
2026-05-06 15:14:30 +0700

    thêm quản lý khách hàng

--- import-service/src/main/java/com/example/importservice/repository/ImportDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.importservice.repository;
-
-import com.example.importservice.entity.ImportDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImportDetailRepository extends JpaRepository<ImportDetail, String> {
-}
--- import-service/src/main/java/com/example/importservice/repository/ImportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.importservice.repository;
-
-import com.example.importservice.entity.ImportTicket;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImportRepository extends JpaRepository<ImportTicket, String> {
-}
